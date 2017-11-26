59a261d5b1dbd3c25b065e7cbdec6e13ec5a09d5
carloscarvallo <carloscarvallo87@gmail.com>
2017-11-25 23:03:14 -0300

    adding types for crawlers

--- types/types.go
+++ types/types.go
@@ -5,6 +5,14 @@ import "gopkg.in/mgo.v2/bson"
 // Project represents a project.
 type Project struct {
 	ID          bson.ObjectId `bson:"_id" json:"_id"`
+	Name        string        `json:"name" bson:"name"`
+	Description string        `json:"description" bson:"description"`
+}
+
+// Crawler represents a crawler.
+type Crawler struct {
+	ID          bson.ObjectId `bson:"_id" json:"_id"`
+	ProjectID   bson.ObjectId `bson:"project_id" json:"project_id"`
 	Jobs        []Job         `bson:",omitempty" json:",omitempty"`
 	Name        string        `json:"name" bson:"name"`
 	Description string        `json:"description" bson:"description"`
